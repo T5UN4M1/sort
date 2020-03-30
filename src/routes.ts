41acc0e126a88a1993d7640bb3c0507692d691a1
Rémi Moustey <remimoustey@gmail.com>
2020-03-30 13:36:44 +0200

    routes.ts : setSortedNumbers()

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,22 @@
-import * as path from "path";
+import path from "path";
+import arrayShuffle from "array-shuffle";
 
+const setSortedNumbers = (lengthArray: number) :number[] => {
+    const numbers:number[] = new Array(Number(lengthArray));
+    for(let i = 0; i < numbers.length; i++) {
+        numbers[i] = i;
+    }
+    return numbers;
+}
 
 const setRoutes = app => {
-    app.get("/getNumbers", (req,res) =>{
+    app.get("/getNumbers", (req,res) => {
         console.log("Not Implemented yet");
     });
-    app.get('*', (req,res) =>{
+    app.get('/randomly-sorted-numbers/:number', (req, res) => {
+        res.send(arrayShuffle(setSortedNumbers(Number(req.params.number))));
+    });
+    app.get('*', (req,res) => {
         res.sendFile(path.join(__dirname,"/../client/build/index.html"));
     });
 };
