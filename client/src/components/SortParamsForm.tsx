4dee31bca3d2d976cef8af97bb6f92439b28daae
T5UN4M1 <adrienabts@gmail.com>
2020-06-24 22:40:18 +0200

    added some data for radiofields and select improvements

--- client/src/components/SortParamsForm.tsx
+++ client/src/components/SortParamsForm.tsx
@@ -17,6 +17,16 @@ const onClick = () => {
 
 };
 
+
+const sortAlgorithms = [
+    {name: 'selection', label: 'Selection'}
+];
+
+const numbersConfigurations = [
+    {name: 'random', label: 'Randomly sorted'},
+    {name: 'almost-sorted', label: 'Almost sorted'},
+    {name: 'inverted-sort', label: 'Inverted sort'}
+];
 const options = () => (
     <OptionField value="insertion">Insertion</OptionField>
 );
@@ -26,8 +36,10 @@ export const Form = () => (
         <NumberField name="quantity" label="quantity" onChange={onChange} value={null} min={50} max={500}/>
         <p>Initial configuration</p>
         <RadioField name="initial-numbers-configuration" id="random" onChange={onChangeRadio} value="random"/>
-        <RadioField name="initial-numbers-configuration" id="almost-sorted" onChange={onChangeRadio} value="almost-sorted"/>
-        <RadioField name="initial-numbers-configuration" id="inverted-sort" onChange={onChangeRadio} value="inverted-sort"/>
+        <RadioField name="initial-numbers-configuration" id="almost-sorted" onChange={onChangeRadio}
+                    value="almost-sorted"/>
+        <RadioField name="initial-numbers-configuration" id="inverted-sort" onChange={onChangeRadio}
+                    value="inverted-sort"/>
         <Select name="algorithm" label="Algorithm" option={options}/>
         <button type="submit" onClick={onClick}>Send</button>
     </form>
