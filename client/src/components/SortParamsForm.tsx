f095a5d064742d0c7c84df123a16734a64156e39
Rémi Moustey <remimoustey@gmail.com>
2020-06-24 19:55:43 +0200

    First try form

---
+++ client/src/components/SortParamsForm.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import {NumberField} from "./NumberField";
+import {RadioField} from "./RadioField";
+import {Select} from "./Select";
+import {OptionField} from "./Option";
+
+
+const onChange = () => {
+
+};
+
+const onChangeRadio = () => {
+
+};
+
+const onClick = () => {
+
+};
+
+const options = () => (
+    <OptionField value="insertion">Insertion</OptionField>
+);
+
+export const Form = () => (
+    <form method="post" action="#">
+        <NumberField name="quantity" label="quantity" onChange={onChange} value={null} min={50} max={500}/>
+        <p>Initial configuration</p>
+        <RadioField name="initial-numbers-configuration" id="random" onChange={onChangeRadio} value="random"/>
+        <RadioField name="initial-numbers-configuration" id="almost-sorted" onChange={onChangeRadio} value="almost-sorted"/>
+        <RadioField name="initial-numbers-configuration" id="inverted-sort" onChange={onChangeRadio} value="inverted-sort"/>
+        <Select name="algorithm" label="Algorithm" option={options}/>
+        <button type="submit" onClick={onClick}>Send</button>
+    </form>
+);
